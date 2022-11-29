818f8c2b4623f66d5113237c36abec1e6c36a902
Rodrigo Thauã Lima Barroso <rodrigothaua@gmail.com>
2022-11-29 17:24:41 -0400

    web criação de pagina links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Sobre from './pages/Sobre'
+import Links from './pages/Links'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // Rotas
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sobre' element={<Sobre />} />
+        <Route path='/links' element={<Links />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
